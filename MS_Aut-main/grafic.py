8ab0dd2653b1b11ba013c73e6f245aa3171b2fa8
Dieg0MV <diegoma.v2004@gmail.com>
2024-08-04 13:39:38 -0600

    update UI

--- MS_Aut-main/grafic.py
+++ MS_Aut-main/grafic.py
@@ -1,18 +1,25 @@
 from tkinter import ttk
 from tkinter import *
 from Ms_aut import *
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
 
 root = Tk()
 root.config(width=500, height=400)
 frm = ttk.Frame(root, padding=30)
 frm.grid()
-def mss():
-    MSsingle
+
+def send():
+    driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    start_url = 'https://web.whatsapp.com'
+    driver.get(start_url)
+    wait = WebDriverWait(driver, 30)
+    
 
 ttk.Label(frm, text="MS_AUT").grid(column=0, row=0)
-ttk.Button(frm, text="new menssage", command=mss).grid(column=1, row=1)
-
-ttk.Button(frm, text="agregar imagen", command=filedialog.askopenfilename()).grid(column=2, row=1)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=2)
+ttk.Button(frm, text="new menssage", command=MSsingle).grid(column=2, row=1)
+ 
+ttk.Button(frm, text="agregar imagen", command=filedialog.askopenfilename()).grid(column=3, row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=4)
 root.mainloop()
 
