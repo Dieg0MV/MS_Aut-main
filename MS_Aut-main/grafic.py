5d78090c936fcbb1b63d2bdda0cbed86b05993b2
Dieg0MV <diegoma.v2004@gmail.com>
2024-08-07 11:53:18 -0600

    update

--- MS_Aut-main/grafic.py
+++ MS_Aut-main/grafic.py
@@ -1,25 +1,78 @@
 from tkinter import ttk
 from tkinter import *
 from Ms_aut import *
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 
+
+
 root = Tk()
-root.config(width=500, height=400)
-frm = ttk.Frame(root, padding=30)
+root.title("Sender")
+root.geometry("400x300")
+frm = ttk.Frame(root, padding=10)
 frm.grid()
 
 def send():
+    message.get("1.0", "end-1c")
+    filename = file_path.get()
+    
     driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
     start_url = 'https://web.whatsapp.com'
     driver.get(start_url)
     wait = WebDriverWait(driver, 30)
     
+    excelFile = xlrd.open_workbook(filename)
+    sheet1 = excelFile.sheet_by_index(0)
+    for i in range(1, sheet1.nrows):    
+        numeros = sheet1.cell_value(rowx=i, colx=1)
+        if not numeros:
+            print("listo")
+            break
+        file_s = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div[2]/div[2]/div/div[1]/p')))
+        time.sleep(5)
+         
+        file_s.send_keys(int(numeros))
+        time.sleep(1)
+        file_s.send_keys(Keys.ENTER)
+        # Esperar unos segundos para asegurarse de que se haya abierto la conversació
+        time.sleep(2)
+    
+        chat_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p')
+
+        # Esperar unos segundos para que el mensaje se envíe antes de pasar al siguiente número
+        time.sleep(1)
+        message = message
+        chat_box.send_keys(message)
+        chat_box.send_keys(Keys.ENTER)
+        time.sleep(5)
+                
+        # Hacer clic en el botón de adjuntar archivo
+        attach_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div/div/span')))        
+        attach_button.click()
+
+
+def select_file():
+    file_path.set(filedialog.askopenfilename())
+
+
 
 ttk.Label(frm, text="MS_AUT").grid(column=0, row=0)
-ttk.Button(frm, text="new menssage", command=MSsingle).grid(column=2, row=1)
+ttk.Button(frm, text="", ).grid(column=2, row=1)
  
-ttk.Button(frm, text="agregar imagen", command=filedialog.askopenfilename()).grid(column=3, row=1)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=4)
+ttk.Label(frm, text="Ingresa tu mensaje").grid(column=0, row=2, pady=(10, 0))
+
+
+textExample = ttk.Entry(frm)
+textExample.grid(column=0, row=3, columnspan=4, pady=(5, 10))
+
+ttk.Button(frm, text="Seleccionar archivo de Excel", command=select_file).grid(column=0, row=2, pady=(5, 0))
+file_path = StringVar()
+ttk.Entry(frm, textvariable=file_path, width=40).grid(column=0, row=3, columnspan=4, pady=(5, 10))
+
+
+
+ttk.Button(frm, text="Send", command=send).grid(column=0, row=4, pady=(5, 0))
+
 root.mainloop()
 
